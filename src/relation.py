2c0c7fcd2fda41f5985d3968dd9c98ed3f9ffc0e
devchait <devchait@gmail.com>
2020-06-20 10:22:41 +0530

    clean everything

--- src/relation.py
+++
@@ -1,7 +0,0 @@
-class Relation(Object):
-    def __init__(left_entity=None, right_entity=None):
-        self.left_entity = left_entity
-        self.right_entity = right_entity
-
-    def relation():
-        return {"left_entity": self.left_entity, "right_entity": self.right_entity}
